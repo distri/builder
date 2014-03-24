5bad9728aa1e465cfabfedaa6d6ad319fbb8f9cf
Daniel X Moore <yahivin@gmail.com>
2014-03-24 15:27:27 -0700

    :honeybee::dolphin: Updated in browser at strd6.github.io/editor

--- v0.3.4/docs/package.js
+++ v0.3.4/docs/package.js
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html manifest="manifest.appcache?1395700043001">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<script src="https://code.jquery.com/jquery-1.10.1.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/coffee-script/1.6.3/coffee-script.min.js"></script>
-</head>
-<body>
-<script>
-;(function(PACKAGE) {
-var oldRequire = window.Require;
-(function() {
+(function(pkg) {
+  (function() {
   var cacheFor, circularGuard, defaultEntryPoint, fileSeparator, generateRequireFn, global, isPackage, loadModule, loadPackage, loadPath, normalizePath, rootModule, startsWith,
     __slice = [].slice;
 
@@ -163,9 +153,7 @@ var oldRequire = window.Require;
 }).call(this);
 
 //# sourceURL=main.coffee
-var require = Require.generateFor(PACKAGE);
-window.Require = oldRequire;
-require('./main')
+  window.require = Require.generateFor(pkg);
 })({
   "source": {
     "LICENSE": {
@@ -770,6 +758,3 @@ require('./main')
     }
   }
 });
-</script>
-</body>
-</html>
